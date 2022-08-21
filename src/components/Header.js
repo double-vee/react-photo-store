30cf6023cba0e982dd2155cca0f423a8c1b01d2b
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-22 01:10:14 +0200

    Added the Header component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="header">
+      <h2 className="header__title">Pic Me</h2>
+      <i className="header__cart ri-shopping-cart-line ri-fw ri-2x"></i>
+    </header>
+  );
+}
