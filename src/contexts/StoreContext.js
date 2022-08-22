0d10b99fe87788dc874218f48afc6e2f2124584c
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-23 01:17:10 +0200

    Photos data fetched and saved in state inside the context

--- src/contexts/StoreContext.js
+++ src/contexts/StoreContext.js
@@ -1,9 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const StoreContext = createContext();
+const URL =
+  "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
 
 function StoreContextProvider({ children }) {
   const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => setPhotos(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <StoreContext.Provider value={{ photos }}>{children}</StoreContext.Provider>
