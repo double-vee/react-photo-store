c9efa8951b9537148142218e139e5b55ebd018d6
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-22 02:06:43 +0200

    Set up routes and links to photos and cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Cart } from "./views/Cart";
 import { Photos } from "./views/Photos";
@@ -6,6 +7,10 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <Routes>
+        <Route path="/" element={<Photos />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <header className="header">
-      <h2 className="header__title">Pic Me</h2>
-      <i className="header__cart ri-shopping-cart-line ri-fw ri-2x"></i>
+      <nav className="header__nav">
+        <Link to="/">
+          <h2 className="header__nav__title">Pic Me</h2>
+        </Link>
+        <Link to="cart">
+          <i className="header__nav__cart ri-shopping-cart-line ri-fw ri-2x"></i>
+        </Link>
+      </nav>
     </header>
   );
 }
