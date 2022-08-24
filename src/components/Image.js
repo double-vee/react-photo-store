4cb0362f7be18ef4f469df338a946274d2a3f49a
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-25 01:38:23 +0200

    Displayed the fav and add icons on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 export function Image({ className, url }) {
   const [isHovered, setIsHovered] = useState(false);
 
+  const heartIcon = isHovered && <i className="ri-heart-line favorite"></i>;
+  const addIcon = isHovered && <i className="ri-add-circle-line add"></i>;
+
   return (
     <div
       onMouseEnter={() => setIsHovered(true)}
@@ -10,6 +13,8 @@ export function Image({ className, url }) {
       className={`${className ? className : ""} image-container`}
     >
       <img src={url} alt="" className="image" />
+      {heartIcon}
+      {addIcon}
     </div>
   );
 }
