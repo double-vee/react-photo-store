ead2a9b17f52198be07f1aa57e5499f0e49e4a39
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-27 01:42:22 +0200

    Mapped over cart items and displayed them in the Cart component; adjusted styles

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { StoreContext } from "../contexts/StoreContext";
+
 export function Cart() {
+  const { cartItems } = useContext(StoreContext);
+
+  const items = cartItems.map((item) => (
+    <div key={item.id} className="cart-item">
+      <img src={item.url} alt="" className="cart__image image" />
+      <p className="cart__price">$6</p>
+    </div>
+  ));
+
   return (
     <main className="cart">
       <h1 className="cart__title">Check out</h1>
+      {items}
     </main>
   );
 }
