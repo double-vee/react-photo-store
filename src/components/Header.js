914633f49ede5771c38eb9ea5120e8cb702451f0
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-27 00:43:34 +0200

    In Header, changed the cart icon to full when an item has been added to the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { StoreContext } from "../contexts/StoreContext";
 
 export function Header() {
+  const { cartItems } = useContext(StoreContext);
+
   return (
     <header className="header">
       <nav className="header__nav">
@@ -8,7 +12,11 @@ export function Header() {
           <h2 className="header__nav__title">Pic Me</h2>
         </Link>
         <Link to="cart">
-          <i className="header__nav__cart ri-shopping-cart-line ri-fw ri-2x"></i>
+          <i
+            className={`header__nav__cart ri-shopping-cart-${
+              cartItems.length > 0 ? "fill" : "line"
+            } ri-fw ri-2x`}
+          ></i>
         </Link>
       </nav>
     </header>
