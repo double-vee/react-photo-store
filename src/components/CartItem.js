0ba058adbe239042ba26e570abaa3e6e6c58b0bd
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-31 00:25:42 +0200

    Enabled removing items from Cart by clicking the bin icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import { StoreContext } from "../contexts/StoreContext";
+
 export const CartItem = ({ id, url }) => {
+  const { removeFromCart } = useContext(StoreContext);
+
   return (
     <div className="cart-item">
       <img src={url} alt="" className="cart__image image" />
       <div className="cart__details">
         <p className="cart__price">$6</p>
-        <i className="ri-delete-bin-line cart__btn--delete"></i>
+        <i
+          onClick={() => removeFromCart(id)}
+          className="ri-delete-bin-line cart__btn--delete"
+        ></i>
       </div>
     </div>
   );
