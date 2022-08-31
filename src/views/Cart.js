049c61424e5912282b922e46307d072a00a9191c
Weronika Kansy <weronika.kansy@gmail.com>
2022-08-31 23:02:15 +0200

    Conditionally rendered elements in Cart: with zero items, displayed an empty cart note and a link to homepage

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { StoreContext } from "../contexts/StoreContext";
 import { CartItem } from "../components/CartItem";
+import { Link } from "react-router-dom";
 
 export function Cart() {
   const { cartItems } = useContext(StoreContext);
@@ -10,9 +11,20 @@ export function Cart() {
   return (
     <main className="cart">
       <h1 className="cart__title">Check out</h1>
-      <div className="cart__items">{items}</div>
-      <p className="cart__total-cost">Total: $0</p>
-      <button className="cart__btn--order">Place order</button>
+      {items.length > 0 ? (
+        <div className="cart__items">
+          {items}
+          <p className="cart__total-cost">Total: $0</p>
+          <button className="cart__btn--order">Place order</button>
+        </div>
+      ) : (
+        <div className="cart__items">
+          <p className="cart__empty">
+            Your cart is empty. <Link to="/">Click here to start shopping</Link>
+            .
+          </p>
+        </div>
+      )}
     </main>
   );
 }
